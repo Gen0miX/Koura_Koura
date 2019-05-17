dd134b03fc4cda0f883b4cef514b558e85e8c949
jonas <jonas.pilloud@students.hevs.ch>
2019-05-17 17:36:21 +0200

    Layout ok

---
+++ app/src/main/java/com/example/koura_koura/RapportOk.java
@@ -0,0 +1,13 @@
+package com.example.koura_koura;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RapportOk extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rapport_ok);
+    }
+}
