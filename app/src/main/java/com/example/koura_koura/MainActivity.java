bacca270300ba0e0a803262bd3de4e542266a981
jonas <jonas.pilloud@students.hevs.ch>
2019-05-17 18:21:34 +0200

    MainActivity ok

--- app/src/main/java/com/example/koura_koura/MainActivity.java
+++ app/src/main/java/com/example/koura_koura/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.koura_koura;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button qualityButton = findViewById(R.id.qualityButton);
+        Button transformationButton = findViewById(R.id.transformationButton);
+
+        qualityButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent quality = new Intent(MainActivity.this, Maladie.class);
+                startActivity(quality);
+            }
+        });
+
+        transformationButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent transformation = new Intent(MainActivity.this, Transformation.class);
+                startActivity(transformation);
+            }
+        });
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/koura_koura/Maladie.java
+++ app/src/main/java/com/example/koura_koura/Maladie.java
@@ -2,8 +2,21 @@ package com.example.koura_koura;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class Maladie extends AppCompatActivity {
+
+    private static final int HOME = 0 ;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+
+        menu.add(0, HOME, Menu.NONE, "Home")
+                .setIcon(R.drawable.ic_home_white_24dp)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        return true ;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
